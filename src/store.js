0148c59be3210732293cb47a591884aa497d8d7e
mr-Dev-Anshu <anshuies31@gmail.com>
2024-12-05 00:35:25 +0530

    channel

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
-
+import channelReducer from './features/channel/channelSlice'
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    channel:channelReducer
   },
 });
 
